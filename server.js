1df4614c9fbee63b3540abe19fa9f06c251aba2e
adicoding-js <kdr5650@gmail.com>
2026-04-14 12:56:44 +0530

    Server: Added Connection handler + canvas state init + emit init to new clients

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ var io = socketio(server);
 
 app.use(express.static("public"));
 
+var canvasState = [];
+var pixelAuthors = [];
+for (var i =0; i < 40000; i++) {
+    canvasState.push("#ffffff");
+    pixelAuthors.push("");
+}
+
+io.on("connection", function(socket) {
+    console.log("connected: " + socket.id);
+    socket.emit("init", {canvasState: canvasState, pixelAuthors: pixelAuthors});
+    
+    socket.on("disconnect", function() {
+        console.log("disconnected: " + socket.id);
+    });
+});
+
 server.listen(3000, function() {
 console.log("server started on port 3000");
 });
